d1eb597cba8926fa19a330f1281ed1d2019ec976
its-robii <robiulhassanrobi33@gmail.com>
2025-08-20 20:23:01 +0600

    create hero section

--- src/components/Nabvar.jsx
+++ src/components/Nabvar.jsx
@@ -1,42 +1,46 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
-import { navData } from '../../constant/index'
+import { navData } from "../../constant/index";
 
 const Navbar = () => {
- useGSAP(() => {
-	const navTween = gsap.timeline({
-	 scrollTrigger: {
-		trigger: 'nav',
-		start: 'bottom top'
-	 }
-	});
-	
-	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
-	 backgroundColor: '#00000050',
-	 backgroundFilter: 'blur(10px)',
-	 duration: 1,
-	 ease: 'power1.inOut'
-	});
- })
- 
- return (
-	<nav>
-	 <div>
-		<a href="#home" className="flex items-center gap-2">
-		 <img src="/images/logo.png" alt="logo" />
-		 <p>Velvet Pour</p>
-		</a>
-		
-		<ul>
-		 {navData.map((link) => (
-			<li key={link.id}>
-			 <a href={`#${link.id}`}>{link.title}</a>
-			</li>
-		 ))}
-		</ul>
-	 </div>
-	</nav>
- )
-}
-export default Navbar
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navData.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
