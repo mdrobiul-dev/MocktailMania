15040d07d4e267b201e64e50f35b6aaf90ed8745
its-robii <robiulhassanrobi33@gmail.com>
2025-08-21 20:41:57 +0600

    artt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Nabvar";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +14,9 @@ function App() {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
+      <About />
+      <Art />
     </main>
   );
 }
